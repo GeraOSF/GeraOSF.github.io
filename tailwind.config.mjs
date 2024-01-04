8daebe2c1e05d79a20924a540b62b51e472cee2f
GeraOSF <gerardogomezromero175@gmail.com>
2024-01-03 20:59:05 -0800

    feat: add header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [nextui()],
 };
