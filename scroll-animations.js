bb5c2db7d2af1807b5ee3700463c11426f77f247
GeraOSF <gerardogomezromero175@gmail.com>
2024-01-03 21:32:19 -0800

    fix(accessibility): reduce animations depending on preference

--- scroll-animations.js
+++ scroll-animations.js
@@ -1,12 +1,21 @@
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("hidden");
-    }
-  });
-});
+const isReduced =
+  window.matchMedia(`(prefers-reduced-motion: reduce)`) === true ||
+  window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
 
 const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach(element => observer.observe(element));
+
+if (isReduced) {
+  hiddenElements.forEach(element => element.classList.remove("hidden"));
+} else {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("hidden");
+      }
+    });
+  });
+
+  hiddenElements.forEach(element => observer.observe(element));
+}
